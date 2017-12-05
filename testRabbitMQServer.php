fd14c35ef51e933e3fb1f596b59ceae63f4b7502
Matthew MIller <mrm36@njit.edu>
2017-12-04 19:15:30 -0500

    Login/Registration working

---
+++ testRabbitMQServer.php
@@ -0,0 +1,74 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('functionLib.inc');
+
+function doLogin($email,$password)
+{
+    $db = new FunctionLib();    
+
+  if(!$db->connect())
+    {	
+	return array("returnCode" => '1', 'message'=>"Error connecting to server");
+    }
+    $data = $db->getUserInfo($email, $password);
+  if($data)
+    {	
+	return (array('returnCode' => '0', 'message' => 'Server received request and processed'));
+    }
+  else
+    {
+	return array("returnCode" => '1', 'message'=>"Login unsuccessful");
+    }
+
+}
+
+function doRegister($request)
+{
+    $db = new FunctionLib();
+    
+    if($db->registerUser($request['email'], $request['password']))
+    {
+	return array("returnCode" => '1', 'message'=>"Registration successful");
+    }
+
+    return array("returnCode" => '0', 'message'=>"<br>Registration unsuccessful<br>Username already exist!");
+}
+
+function logMessage($request)
+{
+	$logFile = fopen("log.txt", "a");
+
+	fwrite($logFile, $request['message'] .'\n\n');
+
+	return true;
+}
+
+function requestProcessor($request)
+{
+  echo "Request Received".PHP_EOL;
+  var_dump($request);
+  echo '\n' . 'End Message';
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "register":
+      return doRegister($request);
+    case "log":
+      return logMessage($request);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+$server->process_requests('requestProcessor');
+exit();
+?>
